b4dab307028617bbc3bcd59b1531bf7a51b2f62b
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2024-09-04 16:43:22 +0800

    添加命令

--- Src/OrzAutoEntity/OrzAutoEntityPackage.cs
+++ Src/OrzAutoEntity/OrzAutoEntityPackage.cs
@@ -25,6 +25,7 @@ namespace OrzAutoEntity
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(OrzAutoEntityPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class OrzAutoEntityPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace OrzAutoEntity
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await AutoEntityCmd.InitializeAsync(this);
         }
 
         #endregion
