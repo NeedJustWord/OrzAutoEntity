5d81d4a83cfa248b0258e6bd30be91dc5153fb56
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2024-09-04 18:08:28 +0800

    添加DTE辅助类和配置文件辅助类

---
+++ Src/OrzAutoEntity/Helpers/ConfigHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace OrzAutoEntity.Helpers
+{
+    public static class ConfigHelper
+    {
+        /// <summary>
+        /// 配置文件是否存在
+        /// </summary>
+        /// <returns></returns>
+        public static bool HasConfigFile()
+        {
+            return File.Exists(GetConfigFullPath());
+        }
+
+        private static string GetConfigFullPath()
+        {
+            var path = DTEHelper.GetSelectedProjectFullPath();
+            return Path.Combine(path, "__entity.xml");
+        }
+    }
+}
---
+++ Src/OrzAutoEntity/Helpers/DTEHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace OrzAutoEntity.Helpers
+{
+    static class DTEHelper
+    {
+        #region DTE2
+        private static DTE2 dte2;
+        internal static DTE2 DTE2
+        {
+            get
+            {
+                if (dte2 == null)
+                {
+                    dte2 = ServiceProvider.GlobalProvider.GetService(typeof(DTE)) as DTE2;
+                }
+                return dte2;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 获取选中的第一个项目
+        /// </summary>
+        /// <returns></returns>
+        public static Project GetSelectedProject()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var items = (Array)DTE2.ToolWindows.SolutionExplorer.SelectedItems;
+            foreach (UIHierarchyItem selItem in items)
+            {
+                if (selItem.Object is Project item && item != null) return item;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 获取选中的第一个项目的全路径
+        /// </summary>
+        /// <returns></returns>
+        public static string GetSelectedProjectFullPath()
+        {
+            var project = GetSelectedProject();
+            return project == null ? string.Empty : project.Properties.Item("FullPath").Value.ToString();
+        }
+    }
+}
